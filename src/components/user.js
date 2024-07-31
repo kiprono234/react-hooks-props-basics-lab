e40c774c7ccb3d49f8277920e2ddfaced900351a
kevin <kipronok234@gmail.com>
2024-07-31 18:37:02 +0300

    react-hooks-props-basic-lab

---
+++ src/components/user.js
@@ -0,0 +1,11 @@
+const user = {
+    name:"Liza",
+    city:"New York",
+    bio:"I made this!",
+    color:"firebrick",
+    links: {
+        github:"https://github.com/liza",
+        linkedin:"https://www.linkedin.com/in/liza"
+    }
+}
+export default user;
